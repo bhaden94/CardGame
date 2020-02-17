a0a72a8b30dc02cecca76c0c79d93c82c8599995
bhade <bhade@DESKTOP-AE0J8AT>
2020-02-17 10:14:59 -0800

    add

---
+++ src/game/Discard.java
@@ -0,0 +1,14 @@
+/**
+ * Discard.java
+ * Implements a discard pile of cards and provides methods
+ * to 
+ *
+ * Brady Haden
+ */
+
+package game;
+
+public class Discard extends GroupOfCards
+{ 
+  // left blank
+}
